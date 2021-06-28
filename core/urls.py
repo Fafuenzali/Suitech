b46de33ce8ad41a323630abdb596521aed2f08e4
Fafuenzali <82009326+Fafuenzali@users.noreply.github.com>
2021-06-28 00:52:48 -0400

    0:49

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,16 @@
 from django.urls import path
-from .views import home
+from .views import home, blake, fireflyxl, fireflym, lc30, helios, new, popular, allp, about
 
 urlpatterns = [
     path('', home, name="home"),
-]
+    path('blake/', blake, name="blake"),
+    path('fireflyxl/', fireflyxl, name="fireflyxl"),
+    path('fireflym/', fireflym, name="fireflym"),
+    path('lc30/', lc30, name="lc30"),
+    path('helios/', helios, name="helios"),
+    path('new/', new, name="new"),
+    path('popular', popular, name="popular"),
+    path('allp', allp, name="allp"),
+    path('about', about, name="about"),
+    ]
+    
--- core/views.py
+++ core/views.py
@@ -3,3 +3,30 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'core/home.html')
+
+def blake(request):
+    return render(request, 'core/blake.html')
+
+def fireflyxl(request):
+    return render(request, 'core/fireflyxl.html')
+
+def fireflym(request):
+    return render(request, 'core/fireflym.html')
+
+def lc30(request):
+    return render(request, 'core/lc30.html')
+
+def helios(request):
+    return render(request, 'core/helios.html')
+
+def new(request):
+    return render(request, 'core/new.html')
+
+def popular(request):
+    return render(request, 'core/popular.html')
+
+def allp(request):
+    return render(request, 'core/allp.html')
+
+def about(request):
+    return render(request, 'core/about.html')
